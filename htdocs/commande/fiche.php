c7f5dd346ee53100ebad91705b2b040cca6a9c3a
Florian HENRY <florian.henry@open-concept.pro>
2014-12-20 11:55:56 +0100

    Fix trad and comment

--- htdocs/commande/fiche.php
+++ htdocs/commande/fiche.php
@@ -2092,6 +2092,7 @@ else
 					$formquestion[] = array('type' => 'hidden','name'=>'line_to_delete[]', 'value'=>$value);
 				}
 			}
+			// TODO: ajax dialog doesn't work because line_to_delete[] can't be find by jquery selector $("#line_to_delete[]") in dialog confirm
 			$formconfirm=$form->formconfirm($_SERVER["PHP_SELF"].'?id='.$object->id, $langs->trans('DeleteObjectLines'), $langs->trans('ConfirmDeleteObjectLines'), 'confirm_delete_object_lines',$formquestion, 0, 0);
 		}
 
