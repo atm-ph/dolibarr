77ec94ca33dc7c93a0409b8aba1c31fcb907fe2f
Florian HENRY <florian.henry@open-concept.pro>
2015-10-14 14:05:16 +0200

    return error message

--- htdocs/compta/sociales/class/chargesociales.class.php
+++ htdocs/compta/sociales/class/chargesociales.class.php
@@ -183,6 +183,7 @@ class ChargeSociales extends CommonObject
                 $result=$accountline->delete_urls($user);
                 if ($result < 0)
                 {
+                	$this->error=$accountline->error;
                     $error++;
                 }
             }
